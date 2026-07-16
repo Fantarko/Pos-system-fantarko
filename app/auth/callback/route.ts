25a561782672b7effcee85fc4e7f80d2cf90d365
Fantarko <fantalopper@gmail.com>
2026-07-16 16:00:42 +0700

    update bug url

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -3,27 +3,42 @@ import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server'
 
 export async function GET(request: Request) {
-  const { searchParams, origin } = new URL(request.url)
+  const { searchParams } = new URL(request.url)
+
   const code = searchParams.get('code')
 
   if (code) {
     const cookieStore = await cookies()
+
     const supabase = createServerClient(
       process.env.NEXT_PUBLIC_SUPABASE_URL!,
       process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
       {
         cookies: {
-          getAll() { return cookieStore.getAll() },
+          getAll() {
+            return cookieStore.getAll()
+          },
+
           setAll(cookiesToSet) {
-            cookiesToSet.forEach(({ name, value, options }) =>
+            cookiesToSet.forEach(({ name, value, options }) => {
               cookieStore.set(name, value, options)
-            )
+            })
           },
         },
       }
     )
-    await supabase.auth.exchangeCodeForSession(code)
+
+    const { error } = await supabase.auth.exchangeCodeForSession(code)
+
+    if (error) {
+      console.log(error)
+      return NextResponse.redirect(
+        new URL('/login?error=auth', request.url)
+      )
+    }
   }
 
-  return NextResponse.redirect(`${origin}/pos`)
+  return NextResponse.redirect(
+    new URL('/pos', request.url)
+  )
 }
